837666a22cbeaa39d1dad9e8ae3e5cfaf85e2a5a
Fredrik Rodland <git@rodland.no>
2013-02-12 11:12:14 +0100

    added frontpage cleaned templates/html fixed and added some tests

---
+++ app/controllers/Application.java
@@ -0,0 +1,17 @@
+package controllers;
+
+import org.apache.solr.client.solrj.SolrServerException;
+import play.mvc.Controller;
+import play.mvc.Result;
+import views.html.index;
+
+/**
+ * User: frerodla
+ * Date: 12.02.13
+ * Time: 11:54
+ */
+public class Application extends Controller {
+    public static Result index() throws SolrServerException {
+        return ok(index.render());
+    }
+}
