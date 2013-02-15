660fad72e876d103eedb6c61fd89ae45f148e12b
Fredrik Rodland <git@rodland.no>
2013-02-15 14:00:45 +0100

    Added twitter boostrap GITHUB v2.3.0 (less files)

--- project/Build.scala
+++ project/Build.scala
@@ -4,8 +4,8 @@ import play.Project._
 
 object ApplicationBuild extends Build {
 
-  val appName         = "solrdemo"
-  val appVersion      = "1.0-SNAPSHOT"
+  val appName = "solrdemo"
+  val appVersion = "1.0-SNAPSHOT"
 
   val appDependencies = Seq(
     // Add your project dependencies here,
@@ -16,7 +16,9 @@ object ApplicationBuild extends Build {
   )
 
   val main = play.Project(appName, appVersion, appDependencies).settings(
-    // Add your own project settings here      
-  )
+    lessEntryPoints <<= (sourceDirectory in Compile)(base => (
+      (base / "assets" / "stylesheets" / "bootstrap.less") +++
+        (base / "assets" / "stylesheets" / "responsive.less")
+      )))
 
 }
